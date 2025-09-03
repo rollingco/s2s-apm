26809d8f5ba566d0a40a160e76f24840929612db
rollingco <vasiliy.kachalo@gmail.com>
2025-09-03 14:21:38 +0300

    Fix duplicate client key assignment in poll_status.php for clarity

--- Tola/poll_status.php
+++ Tola/poll_status.php
@@ -15,6 +15,7 @@ $API_USER    = 'leogc';
 $API_PASS    = 'ORuIO57N6KJyeJ';
 
 $CLIENT_KEY  = 'a9375384-26f2-11f0-877d-022c42254708';
+$CLIENT_KEY   = 'a9375190-26f2-11f0-be42-022c42254708'; 
 $SECRET      = '554999c284e9f29cf95f090d9a8f3171';
 
 // у доках може бути інша назва дії: STATUS / CHECK / QUERY — підставити правильно
