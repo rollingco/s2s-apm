b48a569b1efdb3a46ce0ec9220e220118a873f67
Vasiliy Kachalo <vasiliy.kachalo@gmail.com>
2025-12-17 00:03:40 +0200

    Update session URL to point to the correct endpoint for MPESA checkout

--- mpesa/mpesa_checkout_session.php
+++ mpesa/mpesa_checkout_session.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $checkoutHost = 'https://pay.leogcltd.com';
-//$sessionUrl   = $checkoutHost . '/api/v1/session';
-$sessionUrl   = $checkoutHost . '';
+$sessionUrl   = $checkoutHost . '/api/v1/session';
+//$sessionUrl   = $checkoutHost . '';
 
 // ✅ ТВОЇ ДАНІ
 $merchantKey   = 'a9375384-26f2-11f0-877d-022c42254708';
