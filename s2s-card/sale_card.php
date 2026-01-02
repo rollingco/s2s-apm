b001f0e7b85b5cbb7e1d5e38bdc1d469d81a8380
Vasiliy Kachalo <vasiliy.kachalo@gmail.com>
2026-01-02 16:08:26 +0200

    Update test card expiration month in sale_card.php to January

--- s2s-card/sale_card.php
+++ s2s-card/sale_card.php
@@ -13,7 +13,7 @@ $secret      = '554999c284e9f29cf95f090d9a8f3171';
 
 // --- Your card data ---
 $cardNumber  = '4111111111111111';
-$expMonth    = '05';
+$expMonth    = '01';
 $expYear     = '2038';
 $cvv         = '123';
 
