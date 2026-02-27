3e8b7bb2054467620a1e0b96d34a8a076b7dda04
Vasiliy Kachalo <vasiliy.kachalo@gmail.com>
2026-02-27 17:15:06 +0200

    Restore original merchant key and password in hosted_checkout.php

--- checkout/hosted_checkout.php
+++ checkout/hosted_checkout.php
@@ -14,8 +14,8 @@ header('Content-Type: text/html; charset=utf-8');
 $CHECKOUT_HOST = 'https://pay.leogcltd.com';
 $SESSION_URL   = $CHECKOUT_HOST . '/api/v1/session';
 
-$merchantKey  = '8af24560-1269-11f1-84a4-2a588e8348b1';
-$merchantPass = '71d7e2e8a5bca26c7cc63776fc36078d';
+$merchantKey  = '5f306e12-0ff2-11f1-bac9-0a9a38974658';
+$merchantPass = '976d5c5d5eacbab78288b12bb15178ba';
 
 /* ===================== INPUTS ===================== */
 $orderNumber      = 'order-' . time();
