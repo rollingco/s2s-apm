3cde6eff42556336e69564c11c277bb0ef823c53
rollingco <vasiliy.kachalo@gmail.com>
2026-03-09 20:20:52 +0200

    111

--- mnt/send_sale_apm.php
+++ mnt/send_sale_apm.php
@@ -11,7 +11,7 @@ header('Content-Type: text/html; charset=utf-8');
 $PAYMENT_URL = 'https://api.leogcltd.com/post-va';
 
 // credentials (as-is from your file)
-$CLIENT_KEY  = 'a9375384-26f2-11f0-877d-022c42254708';
+$CLIENT_KEY  = 'a9375190-26f2-11f0-be42-022c42254708';
 $SECRET      = '554999c284e9f29cf95f090d9a8f3171';
 
 $DEFAULTS = [
