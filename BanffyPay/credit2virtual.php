ba4943244f9d82f76ab3119cc91944eebc2fa3b5
rollingco <vasiliy.kachalo@gmail.com>
2026-05-09 19:04:14 +0300

    Update CLIENT_KEY and SECRET in send_sale_apm and credit2virtual scripts to use new credentials

--- BanffyPay/credit2virtual.php
+++ BanffyPay/credit2virtual.php
@@ -21,8 +21,10 @@ header('Content-Type: text/html; charset=utf-8');
 /* ===================== CONFIG ===================== */
 $PAYMENT_URL = 'https://api.leogcltd.com/post';
 
-$CLIENT_KEY = 'a9375190-26f2-11f0-be42-022c42254708';
-$SECRET     = '554999c284e9f29cf95f090d9a8f3171';
+$CLIENT_KEY = '5f306e12-0ff2-11f1-bac9-0a9a38974658';
+$SECRET     = '976d5c5d5eacbab78288b12bb15178ba';
+//$CLIENT_KEY = 'a9375190-26f2-11f0-be42-022c42254708';
+//$SECRET     = '554999c284e9f29cf95f090d9a8f3171';
 
 $BRAND = 'leogc-bannf-dbm';
 $WITHDRAWAL_PAYMENT_CODE = '999';
