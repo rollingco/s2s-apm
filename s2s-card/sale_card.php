f1ae2b2b81937843e6a824482797e4f8e8b40dbb
Vasiliy Kachalo <vasiliy.kachalo@gmail.com>
2026-09-17 15:41:53 +0300

    Update card details in sale_card.php for testing purposes

--- s2s-card/sale_card.php
+++ s2s-card/sale_card.php
@@ -16,10 +16,10 @@ $merchantKey = '5f306e12-0ff2-11f1-bac9-0a9a38974658';
 $secret      = '976d5c5d5eacbab78288b12bb15178ba';
 
 // Card
-$cardNumber  = '4441111087875187';
-$expMonth    = '03';
-$expYear     = '2030';
-$cvv         = '501';
+$cardNumber  = '5100947330319591';
+$expMonth    = '06';
+$expYear     = '2028';
+$cvv         = '177';
 
 // Payer
 $payerEmail  = 'garik.m@pay.cc';
